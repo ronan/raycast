301a03c8b3ad6d457e034b6685f3492e1262b6d0
Ronan Dowling <ronan@pantheon.io>
2021-05-12 18:32:42 -0500

    forgot player.c

--- src/player.c
+++ src/player.c
@@ -1,14 +1,17 @@
 #include "common.h"
 #include "input.h"
 #include "player.h"
+#include "map.h"
+#include "geometry.h"
 
 Player g_player;
 
 void player_init()
 {
-  player_set_pos(5.5, 4);
-  player_set_angle((M_PI_2));
+  player_set_pos(6.5, 1.5);
+  player_set_angle((M_PI_2 + M_PI_4));
   player_set_speed(PLAYER_SPEED);
+  g_player.r = .2;
 }
 
 void player_tick(float t) {
@@ -54,5 +57,24 @@ void player_modify_angle(angle a)
 
 void player_mv(float delta_x, float delta_y)
 {
-  player_set_pos(g_player.pos.x + delta_x, g_player.pos.y + delta_y);
+  Point player_pos = point_add(g_player.pos, (Point){delta_x, delta_y});
+  Point player_int = point_add(player_pos, point_mult(g_player.dir, g_player.r));
+
+  MapTile tile = map_tile_at_point(player_int);
+  if (!map_tile_is_oob(tile) && !map_tile_is_wall(tile))
+    {
+      player_set_pos(player_pos.x, player_pos.y);
+    }
+    else {
+      // Todo. Resolve colision so that the player slides along the wall
+      // float x_diff = player_int.x - player_pos.x;
+      // float y_diff = player_int.y - player_pos.y;
+      // if (x_diff > 1) {
+      //   player_pos.x = player_int.x = player_pos.x + sinf(g_player.ang) * g_player.r;
+      // }
+      // if (x_diff < -g_player.r) {
+      //   player_pos.x = player_int.x = player_pos.x - sinf(g_player.ang) * g_player.r;
+      // }
+
+    }
 }
